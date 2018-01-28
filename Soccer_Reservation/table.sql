f3caca7a5201083d0ed284d93d2412cba2b61136
KangMinGu <mincu1028@gmail.com>
2018-01-28 21:25:19 +0900

    users table sql

---
+++ Soccer_Reservation/table.sql
@@ -0,0 +1,10 @@
+create table users 
+(id varchar2(100) primary key, 
+name varchar2(100), 
+pwd varchar2(100), 
+email varchar2(100), 
+phone varchar2(100), 
+addr varchar2(100), 
+findquestion varchar2(100), 
+findAnswer varchar2(100), 
+regdate date)
