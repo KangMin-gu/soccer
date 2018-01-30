2958b1194861e253032077e682e5d40ef914b541
KangMinGu <mincu1028@gmail.com>
2018-01-30 23:58:30 +0900

    FieldDao 생성
    FieldDao 및 interface 상속

---
+++ Soccer_Reservation/src/main/java/com/soccer/rv/field/dao/FieldDaoImpl.java
@@ -0,0 +1,23 @@
+package com.soccer.rv.field.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.soccer.rv.field.dto.FieldDto;
+
+@Repository
+public class FieldDaoImpl implements FieldDao{
+	
+	@Autowired
+	private SqlSession session;
+
+	@Override
+	public List<FieldDto> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
