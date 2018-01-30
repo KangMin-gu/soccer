00dedfe04c1cfc723dfdee00de42dbbd26e60148
KangMinGu <mincu1028@gmail.com>
2018-01-30 17:08:48 +0900

    운동장 좌표를 얻기위한 table생성
    field 운동장 table 생성

--- Soccer_Reservation/table.sql
+++ Soccer_Reservation/table.sql
@@ -1,10 +1,8 @@
-create table users 
-(id varchar2(100) primary key, 
-name varchar2(100), 
-pwd varchar2(100), 
-email varchar2(200), 
-phone varchar2(200), 
-addr varchar2(200), 
-findquestion varchar2(200), 
-findAnswer varchar2(200), 
-regdate date)
+create table field (field_name varchar2(100) primary key, field_addr varchar2(100));
+
+insert into field values ('서울홍은초등학교', '서울서대문구홍제3동286-5');
+insert into field values ('서울인초등학교', '서울서대문구홍제3동245-1');
+insert into field values ('명지대학교', '서울서대문거북골로34');
+
+
+	
